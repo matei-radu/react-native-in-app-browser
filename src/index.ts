51474723c0ba12c61f453be516ee4cba71faf885
Matei Radu <matei.radu.92@gmail.com>
2019-01-10 18:02:44 +0100

    Add InAppBrowser wrapping object
    `InAppBrowser` class exposes the same functions statically with more
    concise names.
    
    Old functions are still exported without changes other than being
    marked as deprecated.

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,59 @@ import { isUrlValid } from "./utils/validation";
 import { sanitize, initialize, Settings } from "./settings";
 
 /**
+ * In-app browser functionalities.
+ */
+class InAppBrowser {
+  /**
+   * Open a URL in app.
+   *
+   * @param {string} url http(s) URL to open.
+   * @param {Object} settings platform-specific settings for the in-app browsers.
+   *
+   * @throws If the `url` is not a valid http(s) URL.
+   */
+  static async open(url: string, settings?: Settings) {
+    if (!isUrlValid(url)) {
+      throw "Invalid URL";
+    }
+
+    NativeModules.RNInAppBrowser.openInApp(
+      url,
+      sanitize(Platform.OS, settings)
+    );
+  }
+
+  /**
+   * Close the current in app browser instance.
+   *
+   * This feature is iOS only as Chrome Custom Tabs does not support programmatic
+   * dismissal.
+   */
+  static close() {
+    if (Platform.OS === "ios") {
+      NativeModules.RNInAppBrowser.closeInApp();
+    }
+  }
+
+  /**
+   * Configure the platform-specific settings for the in-app browser
+   * experience.
+   *
+   * This utility function is useful when `InAppBrowser.open` is used in several
+   * portions of the application code base as it allows to provide the
+   * settings only once instead of specifing them with each call.
+   */
+  static configure = initialize;
+}
+
+/**
  * Open a URL in app.
  *
  * @param {string} url http(s) URL to open.
  * @param {Object} settings platform-specific settings for the in-app browsers.
  *
  * @throws If the `url` is not a valid http(s) URL.
+ * @deprecated
  */
 async function openInApp(url: string, settings?: Settings) {
   if (!isUrlValid(url)) {
@@ -30,6 +77,8 @@ async function openInApp(url: string, settings?: Settings) {
  *
  * This feature is iOS only as Chrome Custom Tabs does not support programmatic
  * dismissal.
+ *
+ * @deprecated
  */
 function closeInAppInstance() {
   if (Platform.OS === "ios") {
@@ -37,4 +86,4 @@ function closeInAppInstance() {
   }
 }
 
-export { openInApp as default, closeInAppInstance, initialize };
+export { openInApp as default, closeInAppInstance, initialize, InAppBrowser };
