1f02af5e67a78fbf4e7a3be02ec096d9c0b9c726
Matei Radu <opensource@mateiradu.dev>
2019-10-27 13:53:17 +0100

    Overhaul scripts and release management

---
+++ release-manager/command/index.ts
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2018-present, Matei Bogdan Radu <opensource@mateiradu.dev>
+ *
+ * This source code is licensed under the MIT license found in the LICENSE
+ * file in the root directory of this source tree.
+ */
+
+export { publishCommand as publish } from './publish';
